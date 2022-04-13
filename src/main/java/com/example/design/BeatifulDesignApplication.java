40906b0dc7ed09fc4f6cb8639366b99c639039a6
jixinshi <jixinshi@thunisoft.com>
2022-04-13 17:54:59 +0800

    init

---
+++ src/main/java/com/example/design/BeatifulDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatifulDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeatifulDesignApplication.class, args);
+    }
+
+}
